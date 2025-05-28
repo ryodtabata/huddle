2456db7f9aa8d8d07cdb66099e52bb5a6b2150e2
Ryo Tabata <rtabata@alpacasys.com>
2025-05-28 14:07:25 -0600

    home page

--- frontend/huddle/app/index.tsx
+++ frontend/huddle/app/index.tsx
@@ -1,15 +1,14 @@
-import { Text, View } from "react-native";
+
+import { useRouter } from "expo-router";
+import { Button, View, Text } from "react-native";
 
 export default function Index() {
+  const router = useRouter();
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Home screen</Text>
+      <Button title="Go to Profile" onPress={() => console.log('lol u pressed a lil button')} />
     </View>
   );
 }
